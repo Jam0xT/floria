8233e2d3d0278371f4c143b309b71918451447e3
NetheriteJam <netheritejam@gmail.com>
2024-06-07 23:15:51 +0800

    upd

--- src/server/room.js
+++ src/server/room.js
@@ -1,4 +1,5 @@
 const players = {};
+const rooms = {};
 
 export {
 	add,
@@ -6,4 +7,36 @@ export {
 
 function add(socket) {
 	players[socket.id] = socket;
+}
+
+class Room {
+	constructor() {
+		this.id = getNewRoomID();
+	}
+
+	add(socket) {
+
+	}
+
+	remove(socket) {
+
+	}
+}
+
+const charList = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const fixedIdLen = 6;
+
+function getNewRoomID() {
+	const arr = new Uint32Array(1);
+	crypto.getRandomValues(arr);
+	let val = arr[0];
+	let id = '';
+	while ( val > 0 ) {
+		id += charList[val % charList.length];
+		val = (val - val % charList.length) / charList.length;
+	}
+	while ( id.length < fixedIdLen ) {
+		id += '0';
+	}
+	return id;
 }
