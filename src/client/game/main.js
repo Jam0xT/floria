93ea3d455e737079e7d88673aa40a93de7ffa430
NetheriteJam <netheritejam@gmail.com>
2024-07-29 18:44:46 +0800

    client rendering layer change

--- src/client/game/main.js
+++ src/client/game/main.js
@@ -21,21 +21,23 @@ function render() {
 	if ( state.self ) {
 		// 创建不同图层
 		const backgroundCtx = canvas.getTmpCtx();
-		const entityCtx = canvas.getTmpCtx();
+		const playerCtx = canvas.getTmpCtx();
+		const petalCtx = canvas.getTmpCtx();
 
 		renderBackground(backgroundCtx, state.self.x, state.self.y);
-		renderPlayer(entityCtx, state.self, state.self);
+		renderPlayer(playerCtx, state.self, state.self);
 		state.entities.forEach(e => {
 			if ( e.type == 'player' ) {
-				renderPlayer(entityCtx, state.self, e);
+				renderPlayer(playerCtx, state.self, e);
 			} else if ( e.type == 'petal' ) {
-				renderPetal(entityCtx, state.self, e);
+				renderPetal(petalCtx, state.self, e);
 			}
 		});
 
 		// 按顺序渲染不同图层
 		canvas.draw(backgroundCtx, canvas.ctxMain);
-		canvas.draw(entityCtx, canvas.ctxMain);
+		canvas.draw(petalCtx, canvas.ctxMain);
+		canvas.draw(playerCtx, canvas.ctxMain);
 	}
 	animationFrameRequestID = requestAnimationFrame(render);
 }
