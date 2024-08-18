ee10fd00ff33eb7dc1be5472ac0a9e7cbf410a29
Air2999 <user_202248@outlook.com>
2024-08-18 13:43:47 +0800

    new-room

--- floria/README.md
+++
@@ -1,2 +0,0 @@
-# Floria.io
-An imitation and modification of the multiplayer web game florr.io.
--- floria/src/server/game/mob.js
+++
@@ -1,9 +0,0 @@
-import Entity from './entity.js';
-
-class Mob extends Entity {
-	constructor() {
-		super();
-	}
-};
-
-export default Mob;
